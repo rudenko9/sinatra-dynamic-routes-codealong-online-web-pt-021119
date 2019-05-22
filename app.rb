379904a3987beac166e30d969bdc3609197a4d89
Nataliya R <n.rudenko@gmail.com>
2019-05-22 02:54:44 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,7 +19,7 @@ class App < Sinatra::Base
     "Goodbye, #{@user_name}."
   end 
   
-  get "/:operation/:number1/:number2" do
+  get '/:operation/:number1/:number2' do
      @operation = params[:operation]
     @num1 = params[:number1].to_i
     @num2 = params[:number2].to_i
